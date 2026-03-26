92966333ec5acb52d7d636f5dd0fae90b731fec2
Eroshin Victor <victor.eroshin.it@gmail.com>
2026-03-26 15:00:41 +0300

    editor signal form

--- src/app/components/post-edit/post-edit.component.ts
+++ src/app/components/post-edit/post-edit.component.ts
@@ -5,23 +5,58 @@ import {
   OnInit,
   signal,
 } from '@angular/core';
+import { form, FormField, required } from '@angular/forms/signals';
 import { ActivatedRoute } from '@angular/router';
+import { IPost } from '../../share/post.interface';
+import { NgStyle, NgClass } from '@angular/common';
+import { PostService } from '../../services/post.service';
 
 @Component({
   selector: 'app-post-edit',
   templateUrl: './post-edit.component.html',
   styleUrls: ['./post-edit.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [FormField, NgClass],
 })
 export class PostEditComponent implements OnInit {
   id = signal('');
+  isEdit = computed(() => this.id() !== '');
 
-  private _isEdit = computed(() => this.id() !== '');
+  postModel = signal<IPost>({
+    id: Date.now(),
+    title: '',
+    body: '',
+  });
 
-  constructor(private _route: ActivatedRoute) {}
+  postForm = form(this.postModel, (scheme) => {
+    required(scheme.title, { message: 'Title is required' });
+    required(scheme.body, { message: 'Post message is required' });
+  });
+
+  bodyErrorState = computed(
+    () =>
+      this.postForm.body().errors().length > 0 &&
+      this.postForm.body().touched(),
+  );
+
+  titleErrorState = computed(
+    () =>
+      this.postForm.title().errors().length > 0 &&
+      this.postForm.title().touched(),
+  );
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _postService: PostService,
+  ) {}
 
   ngOnInit(): void {
     this._route.snapshot.paramMap.get('id');
     this.id.set(this._route.snapshot.paramMap.get('id') || '');
+  }
+
+  onSubmit(event: SubmitEvent) {
+    event.preventDefault();
+    this._postService.addPost(this.postModel());
   }
 }
--- src/app/components/post-view/post-view.component.ts
+++ src/app/components/post-view/post-view.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { IPost } from '../../share/post.interface';
 
 @Component({
   selector: 'app-post-view',
@@ -6,4 +7,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./post-view.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PostViewComponent {}
+export class PostViewComponent {
+  post = signal<IPost | null>(null);
+}
